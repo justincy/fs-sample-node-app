6f332d1d1e548d70199c00f5da6a25f220595772
Justin York <justincyork@gmail.com>
2013-02-06 01:08:56 -0700

    Initial setup of sample app

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var fs = require('fs-js-sdk');
+
+// Create express app
+var app = express();
+app.set('view engine', 'ejs');
+
+// Setup FamilySearch API Client
+var fsClient = new fs.Client('');
+
+app.get('/discovery', function(req, res){
+  fsClient.getDiscovery(function(discovery){
+    res.send(discovery);
+  });
+});
+
+// Bind app to port
+var port = process.env.PORT || 3000;
+app.listen(port);
